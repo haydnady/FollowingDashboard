a4c3500c4f44f4b88d04952fdd9adc65c6ee3c9c
Noir <haydnady@gmail.com>
2020-06-19 23:48:44 -0400

    Added youtube logo

--- main.py
+++ main.py
@@ -3,19 +3,19 @@
 """
 
 from PySide2.QtWidgets import QApplication, QMainWindow
-from PySide2 import QtUiTools, QtGui
+from PySide2 import QtUiTools, QtGui, QtWidgets
 from generalFunctions import formatN
 from form import Ui_MainWindow
 from APICalls import *
 import sys
 import os
 
-class MainWindow(QMainWindow):
+class myApp(Ui_MainWindow):
 
-    def __init__(self):
-        super(MainWindow, self).__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+     def __init__(self, window):
+        self.setupUi(window)
+
+        self.labelYoutube.setPixmap(os.path.abspath("resources\\youtubeLogo.png"))
 
 
 
@@ -24,11 +24,14 @@ class MainWindow(QMainWindow):
 if __name__ == "__main__":
     # Create the Qt Application
     app = QApplication(sys.argv)
-    # Create and show the form
-    window = MainWindow()
-    window.show()
-    # Run the main Qt loop
-    sys.exit(app.exec_())
+    MainWindow = QMainWindow()
+
+    # Create an instance and show the form
+    ui = myApp(MainWindow)
+
+    # show the window and start the app
+    MainWindow.show()
+    app.exec_()
 
 
 # For testing purposes.
